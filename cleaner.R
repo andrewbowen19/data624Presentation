a4cfbe4023ffef07aa15f8cde6c429dfb2603b68
andrewbowen19 <atbowen@gmail.com>
2024-02-04 15:35:49 -0500

    Update cleaner

--- cleaner.R
+++ cleaner.R
@@ -3,9 +3,10 @@ library(tsibble)
 library(lubridate)
 library(glue)
 
-# Read in raw data: 
-# download link: https://s3.amazonaws.com/tripdata/JC-202312-citibike-tripdata.csv.zip
+# Reading in and aggregating raw Citibike ride data fro Jersey City in Q4 2023
+# Data download link: https://s3.amazonaws.com/tripdata/index.html
 
+# Read in raw data
 dat1 <- read_csv("data/JC-202312-citibike-tripdata.csv")
 dat2 <- read_csv("data/JC-202311-citibike-tripdata.csv")
 dat3 <- read_csv("data/JC-202310-citibike-tripdata.csv")
@@ -14,11 +15,12 @@ dat3 <- read_csv("data/JC-202310-citibike-tripdata.csv")
 dat <- rbind(dat1, dat2, dat3)
 df <- dat %>% select(rideable_type, started_at, member_casual, start_station_name)
 
-# Count numbe rof rides started per station, ride, and membership type per hour over the month
+# Count number of rides started per station, ride, and membership type per hour over the month
 rides_per_hour <- df %>%
   group_by(time=floor_date(started_at, '1 hour'), start_station_name, rideable_type, member_casual) %>%
   summarize(number_of_rides=n())
 
+# Write to local CSV
 write.csv(rides_per_hour, "data/rides_per_hour.csv")
 
 
