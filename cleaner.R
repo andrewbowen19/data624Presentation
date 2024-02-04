1cc18b1212204bc1418648e6e965953e48f119aa
andrewbowen19 <atbowen@gmail.com>
2024-02-04 14:51:39 -0500

    add CSV writer

--- cleaner.R
+++ cleaner.R
@@ -10,15 +10,18 @@ rides_per_hour <- library(tidyverse)
 library(tsibble)
 library(lubridate)
 
+# Read in raw data
 dat <- read_csv("/Users/andrewbowen/Downloads/JC-202312-citibike-tripdata.csv")
 
 
 df <- dat %>% select(rideable_type, started_at, member_casual, start_station_name)
 
-
-df %>%
+# Count numbe rof rides started per station, ride, and membership type per hour over the month
+rides_per_hour <- df %>%
   group_by(time=floor_date(started_at, '1 hour'), start_station_name, rideable_type, member_casual) %>%
   summarize(number_of_rides=n())
 
+
+write.csv(rides_per_hour, "data/rides_per_hour.csv")
 
 
